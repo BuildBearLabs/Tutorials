a494ff68b6cc721da8c8041418cfd5e4e2a4f08d
paritomarrr <pari2019cse083abesit@gmail.com>
2022-04-02 22:32:18 +0530

    mintNFT

--- NFT-verse/contracts/NFT.sol
+++ NFT-verse/contracts/NFT.sol
@@ -7,6 +7,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 // Function counter increments/decrement based on token minted
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+
 // our contract NDT inherits from openzeppelin's ERC721 contract
 contract NFT is ERC721 {
 
@@ -15,12 +16,15 @@ contract NFT is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private tokenId;
 
+    // Base Token URI used as a prefix by tokenURI()
+    string public baseTokenURI;
+
     // our constructor function calls the ERC721 
     // passing two arguments for name and symbol respectively
     constructor() ERC721("UVLabs", "UVL") {}
 
     // defined function mint which is taking a valid recipient's address as argument
-    function mint(address recipientAddress, sring memory tokenURI) 
+    function mint(address recipientAddress, string memory tokenURI) 
     public returns (uint256) {
         // increments tokenId value by one
         tokenId.increment();
@@ -28,9 +32,18 @@ contract NFT is ERC721 {
         // _safeMint is a private method 
         // which mints the next value of the counter
         _safeMint(recipientAddress, newItemId);
-        // 'tokenURI' as URI of our NFT's metadata file
-        _setTokenURI(newItemId, tokenURI);
+       
         // returns a newly minted token's ID back to caller
         return newItemId;
+    }
+
+    // returns an URI for a given token ID
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseTokenURI;
+    }
+        // sets the base token URI prefix
+       function setBaseTokenURI(string memory _baseTokenURI) public {
+    baseTokenURI = _baseTokenURI;
+ 
     }
 }
--- NFT-verse/scripts/mint.js
+++ NFT-verse/scripts/mint.js
@@ -1,7 +1,35 @@
 require('dotenv').config();
 require('@nomiclabs/hardhat-ethers');
 
+const { ethers } = require('hardhat');
 // var representing contract's ABI 
 // ABI enables our script to interact with smart contract
 const contract = require('../artifacts/contracts/NFT.sol/NFT.json');
 const contractInterface = contract.abi;
+
+let provider = ethers.provider;
+
+
+const tokenURI = "QmRPYdhsAUuTAYxcrdJYDn9dFNNe7JWmv3tBcumW41q9yW";
+const privateKey = `0x${process.env.PRIVATE_KEY}`;
+const wallet = new ethers.Wallet(privateKey);
+const signer = wallet.connect(provider);
+
+
+const nft = new ethers.Contract(
+    process.env.CONTRACT_ADDRESS,
+    contractInterface,
+    signer
+  );
+  
+  const main = () => {
+    console.log("Waiting 5 blocks for confirmation...");
+    nft
+      .mint(process.env.PUBLIC_KEY, tokenURI)
+      .then((tx) => tx.wait(5))
+      .then((receipt) => console.log(`Your transaction is confirmed, its receipt is: ${receipt.transactionHash}`))
+  
+      .catch((e) => console.log("something went wrong", e));
+  };
+  
+  main();
