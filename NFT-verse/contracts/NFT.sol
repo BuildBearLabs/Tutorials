1ab883fd34374745aa3b3cbb596ee4da05403e8d
paritomarrr <pari2019cse083abesit@gmail.com>
2022-04-04 14:32:40 +0530

    contract update

--- NFT-verse/contracts/NFT.sol
+++ NFT-verse/contracts/NFT.sol
@@ -1,54 +1,43 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.1;
 
-// stores some basic implemented functions for NFTs
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 // Function counter increments/decrement based on token minted
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+// stores some basic implemented functions for NFTs
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
-
-// our contract NDT inherits from openzeppelin's ERC721 contract
-contract NFT is ERC721 {
+// our contract NfT inherits from openzeppelin's ERC721 contract
+contract NFT is ERC721URIStorage {
 
     // importing and then declaring a counter
     // to keep a track of total tokens minted
     using Counters for Counters.Counter;
     Counters.Counter private tokenId;
 
-    // Base Token URI used as a prefix by tokenURI()
-    string public baseTokenURI;
 
     // our constructor function calls the ERC721 
     // passing two arguments for name and symbol respectively
     constructor() ERC721("UVLabs", "UVL") {}
 
-
-
+  
     // defined function mint which is taking a valid recipient's address as argument
-    function mint(address recipientAddress, string memory tokenURI) 
+    function mint(address recipientAddress, string memory tokenURI_) 
     public returns (uint256) {
+        
+   
         // increments tokenId value by one
         tokenId.increment();
         uint256 newItemId = tokenId.current();
         // _safeMint is a private method 
         // which mints the next value of the counter
         _safeMint(recipientAddress, newItemId);
-        
+        // 
+        _setTokenURI(newItemId, tokenURI_);
+
        
         // returns a newly minted token's ID back to caller
         return newItemId;
     }
-
-//  // returns an URI for a given token ID
-//     function _baseURI() internal view virtual override returns (string memory) {
-//         return baseTokenURI;
-//     }
-//         // sets the base token URI prefix
-//        function setBaseTokenURI(string memory _baseTokenURI) public {
-//     baseTokenURI = _baseTokenURI;
- 
-//     }
-   
 }
--- NFT-verse/scripts/mint.js
+++ NFT-verse/scripts/mint.js
@@ -2,12 +2,12 @@ const hre = require('hardhat');
 
 async function main() {
   // tokenURI represents unique identifier of 'token' looks like
-  const tokenURI = "QmaVGuuJWQYEAmAEG5UD2BeMR82ZPMjHqadXbAg9ddLEzt";
+  const tokenURI = "QmWZQJaPHtpRejG2Wc7KJjp1MFwuCepxa4SJ8fBmoyAEBr";
   // created contract factory of NFT contract
   const nftContractFactory = await hre.ethers.getContractFactory("NFT");
   // contract instance created using contract address, contract's abi
   const nftContractInstance = new hre.ethers.Contract(
-    "0x01555d8b14553019E4d4B19b3DAf1C84A6911F7B", //insert contract address that gets deployed
+    "0xC77c013F751883304023bc7195CAAEED670A8427", //insert contract address that gets deployed
     nftContractFactory.interface,
    
   )
@@ -19,10 +19,7 @@ async function main() {
   nftContractInstance.connect(signer).mint(signerAddress, tokenURI)
   .then((e) => console.log(`Your transaction is confirmed! The transaction hash is ${e.hash}`))
   .catch((e) => console.log("something went wrong", e));
-  // nftContractInstance.mint(signerAddress, tokenURI)
-  // .then(console.log(`Your transaction is confirmed!`))
   
-  //     .catch((e) => console.log("something went wrong", e));
 };
 
 
