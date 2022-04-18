24a794d4d63616ba672f95aac8677b4909184748
amateur-dev <me@dipeshsukhani.dev>
2022-04-18 19:35:14 +0800

    update code

--- Mainnet-Forking/hardhat.config.js
+++ Mainnet-Forking/hardhat.config.js
@@ -2,19 +2,19 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
 
+require('@nomiclabs/hardhat-ethers')
+require('dotenv').config();
 
- require('dotenv').config();
-
-const {ALCHEMY_KEY} = process.env
+const { ALCHEMY_KEY } = process.env
 
 module.exports = {
-  solidity: "0.7.3",
-  networks: {
-    hardhat: {
-        forking: {
-            url:`https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
-            blockNumber: 12505760,
+    solidity: "0.7.3",
+    networks: {
+        hardhat: {
+            forking: {
+                url: `https://eth-mainnet.alchemyapi.io/v2/${ALCHEMY_KEY}`,
+                blockNumber: 14608000,
+            },
         },
     },
-},
 };
--- Mainnet-Forking/scripts/transfer.js
+++ Mainnet-Forking/scripts/transfer.js
@@ -1,46 +1,47 @@
-const { BigNumber, ethers } = require("ethers");
+const { BigNumber, ethers } = require("hardhat");
 
 async function main() {
     const abi = [
         // Read-Only Functions
         "function balanceOf(address owner) view returns (uint256)",
-        "function decimals() view returns (uint8)",
-        "function symbol() view returns (string)",
 
         // Authenticated Functions
         "function transfer(address to, uint amount) returns (boolean)",
-
-        // Events
-        "event Transfer(address indexed from, address indexed to, uint amount)",
     ];
 
-    const provider = new ethers.providers.JsonRpcProvider();
+    const vitalik_address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
+    const uniToken_address = "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984";
+    const my_address = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
 
-    // impersonating vitalik's account
-    await provider.send("hardhat_impersonateAccount", [
-        "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B",
-    ]);
 
-    const signer = await provider.getSigner(
-        "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B"
+    // //  impersonating vitalik's account
+    await hre.network.provider.request({
+        method: "hardhat_impersonateAccount",
+        params: [vitalik_address],
+    });
+
+    const signer = await ethers.getSigner(
+        vitalik_address
     );
 
     // uniswap contract
     const uniToken = new ethers.Contract(
-        "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+       uniToken_address,
         abi,
         signer
     );
-    const vitalik_Balance = (await uniToken.balanceOf("0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B")).toString()
-    console.log(vitalik_Balance)
-    // transferring ETH 
-    const tx = await uniToken.transfer(
-        "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
-        BigNumber.from(vitalik_Balance)
-    );
+    const vitalik_Balance = (await uniToken.balanceOf(vitalik_address)).toString()
 
-    console.log(tx);
-    console.log('Balance: ',(await uniToken.balanceOf("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266")).toString());
+    // transferring ETH 
+    await uniToken.transfer(
+        my_address,
+        ethers.BigNumber.from(vitalik_Balance)
+    );
+    
+    if((await uniToken.balanceOf(my_address) == vitalik_Balance)) {
+        console.log(`Wohoo!! You now have ${ethers.utils.formatEther(vitalik_Balance)} UNI Tokens!`)
+    }
+    
 }
 
 main()
