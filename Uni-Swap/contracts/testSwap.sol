3b5784edd578184be3d84d7ffe1d4bea6ea0445b
paritomarrr <pari2019cse083abesit@gmail.com>
2022-04-20 17:53:43 +0530

    swapping contract

---
+++ Uni-Swap/contracts/testSwap.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8;
+
+import "@uniswap/v2-core/contracts/interfaces/IERC20.sol";
+import '../interfaces/Uniswap.sol';
+contract testSwap {
+    
+}
