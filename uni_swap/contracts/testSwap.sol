62c945452550e2bdc03bd1217d71281c72e49718
amateur_dev <me@dipeshsukhani.dev>
2022-04-25 15:30:27 +0800

    refactoring

--- uni_swap/contracts/testSwap.sol
+++ uni_swap/contracts/testSwap.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8;
 
 import "./interfaces/IERC20.sol";
 import "./interfaces/Uniswap.sol";
-import "hardhat/console.sol";
 
 contract testSwap {
     uint256 deadline;
@@ -11,42 +10,38 @@ contract testSwap {
     address private constant UNISWAP_V2_ROUTER =
         0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
-    //address of WETH token- its better to trade through WETH.
-    address private constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
-
-    
 
     // swap function
     function swap(
         address _tokenIn,
         address _tokenOut,
         uint256 _amountIn,
-        uint256 _amountOutMin,
-        address _to
+        address _to,
+        uint256 _deadline
       
     ) external {
         // transfer the amount in tokens from msg.sender to this contract
         IERC20(_tokenIn).transferFrom(msg.sender, address(this), _amountIn);
 
         //by calling IERC20 approve you allow the uniswap contract to spend the tokens in this contract
-        IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, 1000000);
+        IERC20(_tokenIn).approve(UNISWAP_V2_ROUTER, _amountIn);
 
         address[] memory path;
         path = new address[](2);
-        path[0] = _tokenIn; // DAI
-        path[1] = WETH; // WETH
+        path[0] = _tokenIn; 
+        path[1] = _tokenOut; 
 
-        uint256[] memory amounts = IUniswapV2Router(UNISWAP_V2_ROUTER).getAmountsOut(
-            10000,
+        uint256[] memory amountsExpected = IUniswapV2Router(UNISWAP_V2_ROUTER).getAmountsOut(
+            _amountIn,
             path
         );
      
-        uint256[] memory amounts2 = IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
-            amounts[0],
-            amounts[1],
+        uint256[] memory amountsReceived = IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForTokens(
+            amountsExpected[0],
+            (amountsExpected[1]*990)/1000, // accpeting a slippage of 1%
             path,
-            address(this),
-            block.timestamp
+            _to,
+            _deadline
         );
     }
 }
--- uni_swap/test/sample-test.js
+++ uni_swap/test/sample-test.js
@@ -9,19 +9,14 @@ describe("Test Swap", function () {
 
     const DAIAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
     const WETHAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
-    const my_address = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
+    const MyAddress = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
     const DAIHolder = "0x5d38b4e4783e34e2301a2a36c39a03c45798c4dd";
     let TestSwapContract;
-
-    const AMOUNT_IN = 1000000;
-    const AMOUNT_OUT_MIN = 1;
-    const TO = my_address;
 
     beforeEach(async () => {
         const TestSwapFactory = await ethers.getContractFactory("testSwap");
         TestSwapContract = await TestSwapFactory.deploy();
         await TestSwapContract.deployed();
-
     })
 
     it("should swap", async () => {
@@ -30,37 +25,25 @@ describe("Test Swap", function () {
             method: "hardhat_impersonateAccount",
             params: [DAIHolder],
         });
-        // deadline
-        // const blockNumBefore = await ethers.provider.getBlockNumber();
-        // const blockBefore = await ethers.provider.getBlock(blockNumBefore);
-        // const timestampBefore = blockBefore.timestamp;
-        // // const DEADLINE = timestampBefore + 1200;
-        // await ethers.provider.send('evm_increaseTime', [120]);
-        // const blockNumAfter = await ethers.provider.getBlockNumber();
-        // const blockAfter = await ethers.provider.getBlock(blockNumAfter);
-        // const timestampAfter = blockAfter.timestamp
-        // const DEADLINE = timestampAfter
-
-
-        // DAI contract
         const impersonateSigner = await ethers.getSigner(DAIHolder);
-        const DaiAddressContract = new ethers.Contract(DAIAddress, ERC20ABI, impersonateSigner)
-        const TokenInBal = await DaiAddressContract.balanceOf(impersonateSigner.address)
-        await DaiAddressContract.approve(TestSwapContract.address, AMOUNT_IN)
-        const SwapBal_Init = await DaiAddressContract.balanceOf(TO);
+     
+        // DAI contract
+        const DAIContract = new ethers.Contract(DAIAddress, ERC20ABI, impersonateSigner)
+        const DAIHolderBalance = await DAIContract.balanceOf(impersonateSigner.address)
+        await DAIContract.approve(TestSwapContract.address, DaiHolderBalance)
+        const SwapBal_Init = await DAIContract.balanceOf(MyAddress);
         const IntSwap = ethers.utils.formatUnits(SwapBal_Init)
         console.log(IntSwap, "initially")
 
         await TestSwapContract.connect(impersonateSigner).swap(
             DAIAddress,
             WETHAddress,
-            AMOUNT_IN,
-            AMOUNT_OUT_MIN,
-            TO,
+            DAIHolderBalance,
+            MyAddress,
         )
 
-        const TokenInBal_2 = await DaiAddressContract.balanceOf(impersonateSigner.address)
-        const SwapBal = await DaiAddressContract.balanceOf(TO);
+        const TokenInBal_2 = await DAIContract.balanceOf(impersonateSigner.address)
+        const SwapBal = await DAIContract.balanceOf(MyAddress);
         const IntSwapBal = ethers.utils.formatUnits(SwapBal)
         console.log(`Swapped balance: ${IntSwapBal}`)
         expect(TokenInBal.gt(TokenInBal_2)).to.be.true;
