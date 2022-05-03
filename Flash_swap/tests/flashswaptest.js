f765157f8e0fad3f16dff03cf6f9d70d8d156fd2
paritomarrr <pari2019cse083abesit@gmail.com>
2022-05-03 16:21:46 +0530

    script update

--- Flash_swap/tests/flashswaptest.js
+++ Flash_swap/tests/flashswaptest.js
@@ -10,7 +10,7 @@ describe("Flash Swap Test", function () {
     const USDCAddress = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
     const borrowAmount = 1000000000; // 1000
 
-    beforeEach(async () => {
+    before(async () => {
         const TestFlashSwapFactory = await ethers.getContractFactory("flashSwap");
         TestFlashSwapContract = await TestFlashSwapFactory.deploy();
         await TestFlashSwapContract.deployed();
