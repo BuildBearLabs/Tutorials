60c1e637d86c61ff6f1ae3bfe5715108a681805c
paritomarrr <pari2019cse083@gmail.com>
2022-09-22 14:15:12 +0530

    bugfix

--- Crowdfunding-with-BuildBear/hardhat.config.js
+++ Crowdfunding-with-BuildBear/hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   networks: {
     buildbear: {
       url: "https://backend.buildbear.io/node/affectionate-fermat-d1d223",
-      accounts: ['fb65ea61c393dc095494126d2def8a1551ece1045c6780e488336e8175da1f36']
+      accounts: [process.env.privateKey]
     }
   }
 };
