26a90ba4b4d97b15583ecb9168001f5801e4eca0
amateur_dev <me@dipeshsukhani.dev>
2022-12-07 19:13:08 +0800

    Cleaned up code and working with DAI

--- eip-2612/scripts/sign.js
+++ eip-2612/scripts/sign.js
@@ -1,87 +1,84 @@
+
 const { ethers, BigNumber } = require("ethers");
 
-const DAITokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
-
-
 async function main() {
-  const buildbearProvider=new ethers.providers.JsonRpcProvider("https://ethindia.backend.buildbear.io/node/Illegal_Luminara_Unduli_f158cbf5")
+  const DAITokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
+  const buildbearProvider=new ethers.providers.JsonRpcProvider(process.env.BuildBear_MAINNET_FORK_RPC_URL)
 
-  const myWallet = new ethers.Signer(process.env.MY_PRIVATEKEY);
-  const approvedAccount= new ethers.Signer(process.env.APPROVED_ACCOUNT_PRIVATEKEY);
+  const myWallet = new ethers.Wallet(process.env.MYWALLET_PRIVATE_KEY,buildbearProvider);
+  const approvedAccount= "INSERT_APPROVED_ACCOUNT_ADDRESS_HERE";
   const relayer= new ethers.Signer(process.env.RELAYER_PRIVATEKEY);
 
-  const domainName = "BuildBear" // put your token name 
+  const domainName = "Dai Stablecoin" // put your token name 
   const domainVersion = "1" 
   const chainId = 1 // this is the chain ID of the chain you are using
-  const contractAddress = deployedContractAddress
+  const contractAddress = DAITokenAddress
 
-  const DAITokenConrtact = await hre.ethers.getContractAt("token", DAITokenAddress, deployerSigner);
-  const nonce = await DAITokenConrtact.nonces(myWallet.address)
-  const currentBlockTimeStamp = (await hre.ethers.provider.getBlock("latest")).timestamp;
-  const expiry = (BigNumber.from(currentBlockTimeStamp).add(BigNumber.from(100)));
+  const DAIContractABI = [
+    "function nonces(address owner) view returns (uint256)"
+  ]
+
+  const DAITokenConrtact = new ethers.Contract(DAITokenAddress,DAIContractABI,myWallet);
+  const nonce = (await DAITokenConrtact.nonces(myWallet.address)).toString()
+  const currentBlockTimeStamp = (await buildbearProvider.getBlock("latest")).timestamp;
+  const expiry = (BigNumber.from(currentBlockTimeStamp).add(BigNumber.from(10000000)));
   const allowed = true;
-
-  // pending from here
 
   const domain = {
     name: domainName,
     version: domainVersion,
+    chainId,
     verifyingContract: contractAddress,
-    chainId
   }
 
   const types = {
     Permit: [
       {
-        name: 'owner',
-        type: 'address',
+        name: "holder",
+        type: "address",
       },
       {
-        name: 'spender',
-        type: 'address',
+        name: "spender",
+        type: "address",
       },
       {
-        name: 'value',
-        type: 'uint256',
+        name: "nonce",
+        type: "uint256",
       },
       {
-        name: 'nonce',
-        type: 'uint256',
+        name: "expiry",
+        type: "uint256",
       },
       {
-        name: 'deadline',
-        type: 'uint256',
+        name: "allowed",
+        type: "bool",
       },
     ]
   }
 
-  const values = {
-    owner: ownerSigner.address,
-    spender: spenderSigner.address,
-    value: value,
-    nonce: nonce,
-    deadline: deadline
+  const message = {
+    holder: myWallet.address,
+    spender: approvedAccount,
+    nonce: Number(nonce),
+    expiry: expiry.toString(),
+    allowed
   }
-
-  const signature = await ownerSigner._signTypedData(domain, types, values);
-  console.log(signature);
-
-  const splitSig = (signature) => {
-    // splits the signature to r, s, and v values.
+  
+  await myWallet._signTypedData(domain, types, message).then((signature) => {
     const pureSig = signature.replace("0x", "")
-
     const r = new Buffer.from(pureSig.substring(0, 64), 'hex')
     const s = new Buffer.from(pureSig.substring(64, 128), 'hex')
     const v = new Buffer.from((parseInt(pureSig.substring(128, 130), 16)).toString());
-    console.log(`r: 0x${r.toString('hex')}, s: 0x${s.toString('hex')}, v: ${v}`)  
-  }
 
-  splitSig(signature);
-  
+    console.log("owner: ", myWallet.address),
+    console.log("spender: ", 0xD31950cD762281b9849c33bfa5CE9A42B756155a),
+    console.log("nonce: " ,Number(nonce)),
+    console.log("expiry: ",expiry.toString());
+    console.log("allowed: ",allowed);
+    console.log(`r: 0x${r.toString('hex')}, s: 0x${s.toString('hex')}, v: ${v}`) 
+  });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
