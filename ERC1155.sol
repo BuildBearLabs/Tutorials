d3db18debea66eb704a595ac616c5f2b638b6a2c
chandn0 <91069560+chandn0@users.noreply.github.com>
2023-03-07 13:02:56 +0530

    updating the functions

--- ERC1155.sol
+++ ERC1155.sol
@@ -2,15 +2,19 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GameItem is ERC1155 {
-    uint256 public constant Rock = 1;
-    uint256 public constant Paper = 2;
-    uint256 public constant Scissors = 3;
+contract MyToken is ERC1155, Ownable {
+    constructor() ERC1155("") {}
 
-    constructor() ERC1155("https://ipfs.io/ipfs/bafybeihjjkwdrxxjnuwevlqtqmh3iegcadc32sio4wmo7bv2gbf34qs34a/{id}.json") {
-        _mint(msg.sender, Rock, 1, "");
-        _mint(msg.sender, Paper, 1, "");
-        _mint(msg.sender, Scissors, 1, "");
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
     }
 }
