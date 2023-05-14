db9fcdf8795d7b57816807b1c30316d0b1041f3f
arjun <arjun232001@gmail.com>
2023-05-14 22:43:03 +0530

    Comments added and removed ganache config from hardhat.config.js

--- AllianceUniversity_Arjun_Kumar_VendMachine/contracts/VendSmartContract.sol
+++ AllianceUniversity_Arjun_Kumar_VendMachine/contracts/VendSmartContract.sol
@@ -5,58 +5,66 @@ pragma solidity >=0.8.2 <0.9.0;
 import "./../node_modules/hardhat/console.sol";
 
 contract VendSmartContract {
-    address payable public owner;
-
+    address public owner;
+    address payable public beneficiary;
+    // Structure to represent a product
     struct Product {
-        uint id;
-        string name;
-        uint price;
-        uint quantity;
-        string imageURL;
+        uint id;            // Id of the product
+        string name;        // Name of the product
+        uint price;         // Price of the product
+        uint quantity;      // Quantity of the product
+        string imageURL;    // URL of the product image
     }
 
+    // Structure to represent an item in the cart
     struct cartItem {
         uint id; //Id of the product
-        uint quantity;
+        uint quantity;  // Quantity of the product user will add
     }
 
+    // Mapping to store the carts of different customers
     mapping(address => cartItem[]) public carts;
 
+    // Mapping to store the products
     mapping(uint => Product) public Products;
 
-    //Modifers
+    // Modifier: only allows the contract owner to execute certain functions
     modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner have permission.");
+        require(msg.sender == owner, "Only the owner has permission.");
         _;
     }
 
+    // Modifier: checks if a purchase is valid based on product availability and price
     modifier validPurchase(uint _id) {
         require(Products[_id].price > 0, "Invalid product price.");
         require(Products[_id].quantity > 0, "Product is out of stock.");
         _;
     }
 
+    // Modifier: checks if the customer has enough funds to complete the purchase
     modifier enoughFunds() {
         cartItem[] storage cart = carts[msg.sender];
-
-        uint totalPrice = totalCartAmmount();
+        uint totalPrice = totalCartAmount();
         require(msg.value >= totalPrice, "Insufficient funds");
-
-        _; 
+        _;
     }
 
-    //Event
+    // Event triggered when a product is purchased
     event ProductPurchased(Product, string message);
 
-    constructor() {
-        owner = payable(msg.sender);
+    //Event triggered when owner change beneficiary address
+    event BeneficiaryChanged(address indexed previousBeneficiary, address indexed newBeneficiary);
 
+    //Constructor
+    constructor(address payable _beneficiary) {
+        owner = msg.sender;
+        beneficiary = _beneficiary; //beneficiary address same as owner
         reStock(1, "Coca Cola", 2, 10, "/");
         reStock(2, "lays", 2, 15, "/");
         reStock(3, "5star", 2, 15, "/");
     }
 
-    //Customer
+    // Customer function to purchase a single product
     function purchaseSingleProduct(
         uint _id
     )
@@ -67,27 +75,31 @@ contract VendSmartContract {
     {
         Product storage product = Products[_id];
         product.quantity--;
-        owner.transfer(product.price);
+        beneficiary.transfer(product.price);
         emit ProductPurchased(product, "ProductPurchased");
         return (product.id, product.name, product.price, product.quantity);
     }
 
+    // Customer function to add a product to their cart
     function addToCart(uint _id, uint _quantity) public {
 
         cartItem[] storage cart = carts[msg.sender];
 
-        //if product already in basket
+        // Check if the product is already in the cart
         uint findItem = findProductInCart(cart, _id);
 
         if (findItem != uint(0)) {
+            // If the product is already in the cart, increase the quantity
             cart[findItem].quantity += _quantity;
         }
         else {
+            // If the product is not in the cart, add it as a new item
             cartItem memory newItem = cartItem(_id, _quantity);
             cart.push(newItem);
         }
     }
 
+    // Internal function to find the index of a product in the cart
     function findProductInCart(
         cartItem[] storage cart,
         uint _id
@@ -100,7 +112,8 @@ contract VendSmartContract {
         return uint(0);
     }
 
-    function totalCartAmmount() public view returns (uint) {
+    // Calculates the total amount to be paid for the items in the customer's cart
+    function totalCartAmount() public view returns (uint) {
         cartItem[] storage cart = carts[msg.sender];
         uint totalPrice = 0;
         for (uint i = 0; i < cart.length; i++) {
@@ -113,14 +126,15 @@ contract VendSmartContract {
         return totalPrice;
     }
 
+    // Retrieves the customer's shopping cart and the total amount to be paid
     function getCart() public view returns (cartItem[] memory, uint) {
         cartItem[] storage cart = carts[msg.sender];
-        uint totalPrice = totalCartAmmount();
+        uint totalPrice = totalCartAmount();
         return (cart, totalPrice);
     }
 
 
-
+    // Completes the purchase process by deducting quantities from the inventory, transferring the payment to the owner, and clearing the cart
     function checkOutCart() public payable enoughFunds {
         cartItem[] storage cart = carts[msg.sender];
 
@@ -131,15 +145,17 @@ contract VendSmartContract {
                 "Not enough product available"
             );
             product.quantity -= cart[i].quantity;
-            owner.transfer(product.price * cart[i].quantity);
+            beneficiary.transfer(product.price * cart[i].quantity);
         }
 
-        delete carts[msg.sender];
+        delete carts[msg.sender]; //After checkout delete the cart
     }
 
 
 
-    //Owner of vending machine
+    //ONLY OWNER CAN CALL THIS FUNCTION
+
+    // Allows the contract owner to add or update the details of a product in the inventory
     function reStock(
         uint _id,
         string memory _name,
@@ -150,7 +166,22 @@ contract VendSmartContract {
         Products[_id] = Product(_id, _name, _price, _quantity, _imageURL);
     }
 
-    function withdraw() public onlyOwner {
-        owner.transfer(address(this).balance);
+    function changeBeneficiary(address _newBeneficiary) public onlyOwner {
+        require(_newBeneficiary != address(0), "Invalid new beneficiary address.");
+        emit BeneficiaryChanged(beneficiary, _newBeneficiary);
+        beneficiary = payable(_newBeneficiary);
     }
+
+    function withdraw() public onlyOwner {
+        beneficiary.transfer(address(this).balance); // Allows the contract owner to withdraw the contract's balance
+    }
+
+
+
 }
+
+
+
+
+
+
--- AllianceUniversity_Arjun_Kumar_VendMachine/hardhat.config.js
+++ AllianceUniversity_Arjun_Kumar_VendMachine/hardhat.config.js
@@ -4,19 +4,15 @@ require("@nomicfoundation/hardhat-toolbox");
 module.exports = {
   solidity: "0.8.18",
   networks: {
-    localganache: {
-      url: "HTTP://127.0.0.1:7545",
-      accounts: ["0x82f6fc957142a47acb231a830bc4e0d06a30bb9eac6db9debaa2005ee5e8f206"]
-    },
     hardhat: {},
     buildbear: {
-      url: "https://rpc.buildbear.io/Xenophobic_Eeth_Koth_546d53b5",
-    },
+      url: "[Enter you RPC URL]"
+    }
   },
 
   etherscan: {
     apiKey: {
-      buildbear: "verifyContract",
+      buildbear: "verifyContract"
     },
     customChains: [
       {
@@ -24,9 +20,9 @@ module.exports = {
         chainId: 8654,
         urls: {
           apiURL: "https://rpc.buildbear.io/verify/etherscan/Xenophobic_Eeth_Koth_546d53b5",
-          browserURL: "https://explorer.buildbear.io/Xenophobic_Eeth_Koth_546d53b5",
-        },
-      },
-    ],
-  },
+          browserURL: "https://explorer.buildbear.io/Xenophobic_Eeth_Koth_546d53b5"
+        }
+      }
+    ]
+  }
 };
