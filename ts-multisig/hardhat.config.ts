8e07e1757a36fa598c94ba3e48220d0c33e8f6ca
Nisarg Chaudhari <54911392+nisargsc@users.noreply.github.com>
2023-07-10 21:24:34 +0530

    Add article for multisig wallet tutorial

--- ts-multisig/hardhat.config.ts
+++ ts-multisig/hardhat.config.ts
@@ -17,7 +17,27 @@ const config: HardhatUserConfig = {
   },
   defaultNetwork: "hardhat",
   networks: {
+    buildbear: {
+      url: "https://rpc.buildbear.io/psychological-san-hill-5768d44d", // RPC_URL from buildbear dashboard
+    },
     hardhat: {},
+  },
+  etherscan: { // Copy from bulidbear dashboard
+    apiKey: {
+      buildbear: "verifyContract",
+    },
+    customChains: [
+      {
+        network: "buildbear",
+        chainId: 9753,
+        urls: {
+          apiURL:
+            "https://rpc.buildbear.io/verify/etherscan/psychological-san-hill-5768d44d",
+          browserURL:
+            "https://explorer.buildbear.io/psychological-san-hill-5768d44d",
+        },
+      },
+    ],
   },
 };
 
