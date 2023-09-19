86f93146faa7c031c43cfc5b55227bf31c0ba33d
Ayush <nayaksubham18@gmail.com>
2023-09-20 00:05:35 +0530

    init

--- DutchAuction/contracts/DutchAuction.sol
+++ DutchAuction/contracts/DutchAuction.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.10;
+pragma solidity ^0.8.13;
 
 interface IERC721 {
     function transferFrom(
@@ -10,10 +10,12 @@ interface IERC721 {
 }
 
 contract DutchAuction {
-    uint private constant DURATION = 2 days;
+    uint private constant DURATION = 7 days;
+
     IERC721 public immutable nft;
     uint public immutable nftId;
-    address public immutable seller;
+
+    address payable public immutable seller;
     uint public immutable startingPrice;
     uint public immutable startAt;
     uint public immutable expiresAt;
@@ -27,31 +29,33 @@ contract DutchAuction {
     ) {
         seller = payable(msg.sender);
         startingPrice = _startingPrice;
-        discountRate = _discountRate;
         startAt = block.timestamp;
         expiresAt = block.timestamp + DURATION;
+        discountRate = _discountRate;
 
-        require(
-            _startingPrice >= _discountRate * DURATION,
-            "Discount is greater than the Starting Price Dude!!"
-        );
+        require(_startingPrice >= _discountRate * DURATION, "starting price < min");
 
         nft = IERC721(_nft);
         nftId = _nftId;
     }
 
-
-    function currentPrice() public view returns (uint) {
+    function getPrice() public view returns (uint) {
         uint timeElapsed = block.timestamp - startAt;
         uint discount = discountRate * timeElapsed;
         return startingPrice - discount;
     }
 
-    function buyNow() external payable {
-        require(block.timestamp < expiresAt, "Auction expired! :(");
-        uint price = currentPrice();
-        require(msg.value >= price, "ETH < Price");
+    function buy() external payable {
+        require(block.timestamp < expiresAt, "auction expired");
+
+        uint price = getPrice();
+        require(msg.value >= price, "ETH < price");
+
         nft.transferFrom(seller, msg.sender, nftId);
-        selfdestruct(payable(seller));
+        uint refund = msg.value - price;
+        if (refund > 0) {
+            payable(msg.sender).transfer(refund);
+        }
+        selfdestruct(seller);
     }
 }
