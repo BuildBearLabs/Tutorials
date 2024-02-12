a61a37b62e2e81b042244c4b680eb84850263143
Raad05 <ashabulraad@gmail.com>
2024-02-12 22:17:05 +0600

    added test script and buildbear network

--- eventHub-dApp/Hardhat/hardhat.config.js
+++ eventHub-dApp/Hardhat/hardhat.config.js
@@ -3,13 +3,17 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const { ALCHEMY_RPC_URL, PRIVATE_KEY } = process.env;
+const { ALCHEMY_RPC_URL, PRIVATE_KEY, BUILDBEAR_RPC_URL } = process.env;
 
 module.exports = {
   solidity: "0.8.20",
   networks: {
     sepolia: {
       url: ALCHEMY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    buildbear: {
+      url: BUILDBEAR_RPC_URL,
       accounts: [PRIVATE_KEY],
     },
   },
